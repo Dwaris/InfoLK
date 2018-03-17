8ec3369dcc962cc81a9a7818e46dbcd43919e358
Nils Malina <nils222@outlook.com>
2018-03-17 20:00:13 +0100

    init + insertionsort

---
+++ 02 Algorithmen/20180316/insertionsort.py
@@ -0,0 +1,12 @@
+L = [54,26,93,17,77,31,2,44,55,20]
+
+def insertionSort(L):
+   for i in range(1,len(L)):
+     min = L[i]
+     while i>0 and L[i-1]>min:
+         L[i]=L[i-1]
+         i = i-1
+     L[i]=min
+
+insertionSort(L)
+print(L)
