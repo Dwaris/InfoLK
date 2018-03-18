8c2c4af9b13fb9ded1ac741139aabcbde7625ea1
dwaris <nils222@outlook.com>
2018-03-18 22:44:27 +0100

    bubblesort fix

--- 02 Algorithmen/20180316/insertionsort.py
+++ 02 Algorithmen/20180316/insertionsort.py
@@ -1,7 +1,7 @@
-L = [54,26,93,17,77,31,2,44,55,20]
+L = [1,26,93,17,77,31,2,44,55,20]
 
 def insertionSort(L):
-   for i in range(1,len(L)):
+   for i in range(0,len(L)):
      min = L[i]
      while i>0 and L[i-1]>min:
          L[i]=L[i-1]
